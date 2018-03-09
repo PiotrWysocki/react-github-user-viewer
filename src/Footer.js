c5f504d0f7e6aafd4f648745eae7c79c8d9327e8
Piotr Wysocki <399piotr@wp.pl>
2018-03-09 12:59:24 +0100

    Collected data to objects and then pass to child components. Added tests to the components.

--- src/Footer.js
+++ src/Footer.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
     render(){
+        const footer = this.props.footer;
+
         return(
             <div className="mx-auto bg-dark col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5">
                 <div className="row">
-                    <a href={this.props.repos > 0 ? this.props.reposUrl : null} target="_blank"
+                    <a href={footer.repos > 0 ? footer.reposUrl : null} target="_blank"
                        className="text-center col my-2 text-light footer-link">
                         <p className="mb-0">Repositories</p>
-                        <p className="mb-0">{this.props.repos}</p>
+                        <p className="mb-0">{footer.repos}</p>
                     </a>
-                    <a href={this.props.followers > 0 ? this.props.followersUrl : null} target="_blank"
+                    <a href={footer.followers > 0 ? footer.followersUrl : null} target="_blank"
                        className="text-center col my-2 text-light footer-link">
                         <p className="mb-0">Followers</p>
-                        <p className="mb-0">{this.props.followers}</p>
+                        <p className="mb-0">{footer.followers}</p>
                     </a>
-                    <a href={this.props.following > 0 ? this.props.followingUrl : null} target="_blank"
+                    <a href={footer.following > 0 ? footer.followingUrl : null} target="_blank"
                        className="text-center col my-2 text-light footer-link">
                         <p className="mb-0">Following</p>
-                        <p className="mb-0">{this.props.following}</p>
+                        <p className="mb-0">{footer.following}</p>
                     </a>
                 </div>
             </div>
--- src/Result.js
+++ src/Result.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 export default class Result extends Component {
     render(){
+        const result = this.props.result;
+
         return(
             <div className="mx-auto col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5 view-main">
-                <a href={this.props.htmlUrl.length > 0 ? this.props.htmlUrl : null} target="_blank">
-                    <img className="img-thumbnail rounded mx-auto d-block my-2 avatar" src={this.props.avatar}/>
+                <a href={result.htmlUrl.length > 0 ? result.htmlUrl : null} target="_blank">
+                    <img className="img-thumbnail rounded mx-auto d-block my-2 avatar" src={result.avatar}/>
                 </a>
-                <p className="text-center font-weight-bold mb-2 name-para">{this.props.name}</p>
-                <p className="text-center mb-2 location-para">{this.props.location}</p>
+                <p className="text-center font-weight-bold mb-2 name-para">{result.name}</p>
+                <p className="text-center mb-2 location-para">{result.location}</p>
             </div>
         )
     }
